234440da5a5b75902dcf5195cb88f6ea3263375e
Happuri <happuri@gmx.com>
2014-11-24 10:07:59 +0100

    gitignore, key length in define

--- main.cpp
+++ main.cpp
@@ -2,17 +2,20 @@
 #include <chrono>
 #include "ckeysstorage.h"
 
+#define KEY_SIZE 4096
+
 using namespace std;
 
 int main()
 {
 	//remove("tmp");
-    cKeysStorage keyStorage = cKeysStorage();
+    std::cout << KEY_SIZE << std::endl;
+	cKeysStorage keyStorage = cKeysStorage();
     //for (int i = 0; i < 1; ++i)
        // keyStorage.generate(std::to_string(i));
 
 	//keyStorage.sign("test.txt", 0);
-	keyStorage.GenerateRSAKey(4096);
+	keyStorage.GenerateRSAKey(KEY_SIZE);
 	keyStorage.RSASignFile("test.txt", "test.sig", 0);
 	keyStorage.RSAVerifyFile("test.sig");
 
