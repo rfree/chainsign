2a8102171ef12ba698b1e885601dc6e53746449d
Happuri <happuri@gmx.com>
2014-12-08 10:30:23 +0100

    program options

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <chrono>
 #include <fstream>
 #include <thread>
+#include <string>
+#include <vector>
+#include <cassert>
+
+#include <boost/program_options.hpp>
 #include "ckeysstorage.h"
 #include "cCmdInterp.hpp"
 
@@ -9,55 +14,108 @@
 
 using namespace std;
 
-// usage
-// chainsign --daemon name_of_instance out_dir
-// chainsign --verify-chain 1st_pub_key --move out_dir
-// chainsign --verify sig_file
+enum parser {
+	UNKNOWN_COMMAND = 4,
+	SUCCESS = 0,
+	ERROR = 5
+};
 
-// ./chainsign --daemon my_instance .
-// ./chainsign --verify-chain my_instance-key1.pub --move good_keys
-// ./chainsign --verify-file sig_file
-int main(int argc, char* argv[])
-{
-	std::cout << "start main" << std::endl;
-	
-	if (std::string(argv[1]) == "--daemon")
-	{
-		cCmdInterp cmdInterp("fifo", argv[2]);
-		cmdInterp.setOutDir(std::string(argv[3]));
-		std::cout << "start loop" << std::endl;
-		cmdInterp.cmdReadLoop();
-	}
-	
-	else if (std::string(argv[1]) == "--verify-chain")
-	{
-		cCmdInterp cmdInterp;
-		cmdInterp.setOutDir(std::string(argv[4]));
-		unsigned int ret = cmdInterp.verify(std::string(argv[2]));
-		if (ret == -1)
-			return 2; // keys verification error
-		std::cout << "OK" << std::endl;
-	}
-	
-	else if (std::string(argv[1]) == "--verify-file")
-	{
-		cCmdInterp cmdInterp;
-		return cmdInterp.verifyOneFile(std::string(argv[2]));
-	}
-    //std::cout << KEY_SIZE << std::endl;
-	/*cKeysStorage keyStorage = cKeysStorage();
-	keyStorage.RSAVerifyFile("test.txt.sig", "my_instance");*/
-    //for (int i = 0; i < 1; ++i)
-       // keyStorage.generate(std::to_string(i));
+void printHelp() {
+	cout << "examples: " << endl;
+	cout << "./chainsign --daemon name_of_instance out_dir" << endl;
+	cout << "./chainsign --verify-chain 1st_pub_key --move out_dir" << endl;
+	cout << "./chainsign --verify sig_file" << endl;
 
-	//keyStorage.sign("test.txt", 0);
-	//keyStorage.GenerateRSAKey(KEY_SIZE);
-	//keyStorage.RSASignFile("test.txt", "test.sig", 0);
+	cout << "./chainsign --daemon my_instance ." << endl;
+	cout << "./chainsign --verify-chain my_instance-key1.pub --move good_keys" << endl;
+	cout << "./chainsign --verify-file sig_file" << endl;
 
-    return 0;
+}
+
+int main(int argc, char* argv[]) {
+	try {
+		string opt;
+		namespace po = boost::program_options;
+		po::options_description desc("Options");
+		desc.add_options()
+		("help", "print help messages")
+		("daemon", "[name of instance] [outdir name] run as daemon mode")
+		("verify-chain", "[key.pub] [good_keys] verify keys and move them to good-key")
+		("verify-file", "[sig_file]");
+
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		po::notify(vm);
+
+		try {
+			std::cout << "start main" << std::endl;
+			if (vm.count("help") || argc == 1) {
+				cout << desc << endl;
+				printHelp();
+				return 0;
+			}
+
+			if (vm.count("daemon")) {
+				assert(argc == 4);
+
+				cCmdInterp cmdInterp("fifo", argv[2]);
+				cmdInterp.setOutDir(std::string(argv[3]));
+				std::cout << "start loop" << std::endl;
+				cmdInterp.cmdReadLoop();
+			}
+
+			else if (vm.count("verify-chain")) {
+				assert(argc == 5);
+
+				cCmdInterp cmdInterp;
+				cmdInterp.setOutDir(std::string(argv[3]));
+				unsigned int ret = cmdInterp.verify(std::string(argv[2]));
+				if (ret == -1) return 2; // keys verification error
+				std::cout << "OK" << std::endl;
+			}
+
+			else if (vm.count("verify-file")) {
+				assert(argc == 3);
+				cCmdInterp cmdInterp;
+				return cmdInterp.verifyOneFile(std::string(argv[2]));
+			}
+
+			/*
+
+			 std::cout << KEY_SIZE << std::endl;
+			 cKeysStorage keyStorage = cKeysStorage();
+			 keyStorage.RSAVerifyFile("test.txt.sig", "my_instance");
+			 for (int i = 0; i < 1; ++i)
+			 keyStorage.generate(std::to_string(i));
+			 keyStorage.sign("test.txt", 0);
+			 keyStorage.GenerateRSAKey(KEY_SIZE);
+			 keyStorage.RSASignFile("test.txt", "test.sig", 0);
+			 */
+
+		}
+
+		catch (po::error& e) {
+			std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+			std::cerr << desc << std::endl;
+			return parser::UNKNOWN_COMMAND;
+		}
+	}
+
+	catch (std::exception& e)
+	{
+		std::cerr << "Unhandled Exception reached the top of main: "
+				<< e.what() << ", application will now exit" << std::endl;
+		return parser::ERROR;
+
+	}
+
+	return 0;
 }
 
 // return 0 - OK
 // return 1 - other error (exception)
 // return 2 - keys verification error
 // return 3 - file verification error
+// return 4 - unknown command
+// return 5 - parser error
+
